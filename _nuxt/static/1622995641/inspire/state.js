dbcd1cf423a220d731a4d44ac01a996aa8569368
Karamorf <Karamorf@users.noreply.github.com>
2021-06-06 16:07:23 +0000

    deploy: b6a3e9bc58e64b2e0da5d6cc11b5e39249bd9d60

---
+++ _nuxt/static/1622995641/inspire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsuperstonkquants\u002F_nuxt\u002Fstatic\u002F1622995641",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Fsuperstonkquants\u002F",assetsPath:"\u002Fsuperstonkquants\u002F_nuxt\u002F",cdnURL:a}}}}(null));
